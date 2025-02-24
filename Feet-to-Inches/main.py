86792f43b4596fd0abc06003f1cf72aab55cc7b4
Sionekinikini <sionekinikini@westernwyoming.edu>
2025-02-24 02:10:44 +0000

    Ready for grading

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,26 @@
 #
-# Name
-# Date
+# Sione Kinkini
+# 2/23/25
 # Feet to Inches Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program.
+
+# Conversion
+inches_per_foot = 12
+
+# Main function
+def main():
+    # Get the number of feet
+    feet = int(input('Enter a number of feet:'))
+
+    # Convert to inches
+    print(feet, 'feet equals', feet_to_inches(feet), 'inches.')
+
+# Calulate feet to inches
+def feet_to_inches(feet):
+    return feet * inches_per_foot
+
+# Call the main function
+main()
+
