a3b1cb151e14f3b831a1746bc8787be6069ef52e
Sionekinikini <sionekinikini@westernwyoming.edu>
2025-03-27 03:27:00 +0000

    Ready for grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,14 @@
 #
-# Name
-# Date
+# Sione Kinikini
+# Date 3/26/25
 # Average of Numbers Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Main function
+def calculate_average(filename):
+    with open(filename) as file:
+        numbers = [int(line) for line in file]
+    print(f"The average is: {sum(numbers) / len(numbers)}")
+
+filename = input("Enter the filename: ")
+calculate_average(filename)
