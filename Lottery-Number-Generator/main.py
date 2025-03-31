b021664bbbf919cfe285b1e7db97fea9e0a152d9
Sionekinikini <sionekinikini@westernwyoming.edu>
2025-03-31 01:39:41 +0000

    Ready for grading

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,22 @@
 #
-# Name
-# Date
+# Sione Kinikini
+# 3/30/25
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+import random
+# Initialize an empty list to store the lottery numbers
+lottery_number = []
+
+# Generate seven random numbers in the range 0 to 9
+for _ in range(7):
+    # Generate a random number between 0 and 9
+    number = random.randint(0, 9)
+     # Append the number to the list
+    lottery_number.append(number)   
+
+# Display the contents of the list
+print("Generated lottery number:")
+for number in lottery_number:
+     # Print each number in the list
+    print(number, end=' ')
