6d94ffa856bd9f169c6f8d3f2a84ea256ac64dc5
Sionekinikini <sionekinikini@westernwyoming.edu>
2025-04-14 02:36:58 +0000

    Ready for grading

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,15 @@
 #
-# Name
-# Date
+# Sione Kinikini
+# 4/13/25
 # Pig Latin Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Create the pig latin function with the split feature
+def piglatin():
+    words = input("Input sentence: ").split()
+    for word in words:
+        print(word[1:] + word[:1] + "ay", end = ' ')
+
+# Call the function
+piglatin()
+    
